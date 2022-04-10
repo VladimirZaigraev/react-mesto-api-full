9609f4117bc13f59defd4918be9f396feabea510
Vladimir Zigraev <v.a.zaigraev@mail.ru>
2022-04-10 18:01:14 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const errorHandler = require('./middelwares/errorHandler');
@@ -23,12 +26,30 @@ const {
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://zaigraev.nomoredomains.work',
+    'http://zaigraev.nomoredomains.work',
+  ],
+  methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Cookie'],
+  credentials: true,
+}));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
-
-app.use(express.json());
 
 app.use(requestLogger); // логгер запросов
 
