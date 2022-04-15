e1607f2770a0216dae3c23e09cabb5d623ee0767
Vladimir Zigraev <v.a.zaigraev@mail.ru>
2022-04-15 08:50:38 +0300

    fix base link

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.zaigraev.nomoredomains.work";
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = "https://api.zaigraev.nomoredomains.work";
+// const BASE_URL = 'http://localhost:3000';
 
 const headers = {
   'Accept': 'application/json',
